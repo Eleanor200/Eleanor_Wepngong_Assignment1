a2558578bf706c68858a64d2b1fdd9c9305964fc
Eleanor200 <90443474+Eleanor200@users.noreply.github.com>
2022-09-30 12:37:31 +0200

    Update question2.c

--- question2.c
+++ question2.c
@@ -4,6 +4,7 @@ int main()
 int num, sum=0, rem;
 printf("Enter a positive integer: ");
 scanf("%d", &num);
+// Keep dividing until the number is not zero
 while(num != 0){
 rem  = num % 10;
 sum  = sum + rem;
